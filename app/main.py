dd5a2f5439f525ca33701e9dad7f44ce4ba13ec1
Anzh <anzhrai@gmail.com>
2021-01-10 19:13:51 +0530

    Loading Jason File With abusive words

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from Model.load_abuse import abuse
 
 app = Flask(__name__)
 CORS(app)
@@ -7,8 +8,11 @@ CORS(app)
  
 @app.route('/')
 def infer():
-    return "Tested"
+    return "Tested", 200
  
+ @app.route('/getAbusiveData', methods['POST'])
+ def bad_words():
+     return abuse, 200
  
 if __name__ == "__main__":
     app.run()
