2c3c634fa52dc944b61d7a6def87cf1f1075c6b6
Anzh <anzhrai@gmail.com>
2021-01-14 16:42:02 +0530

    model.py file made and route updated

---
+++ Model/model.py
@@ -0,0 +1,11 @@
+def model(data):
+    print(data)
+    #DO your manupulations here.
+    manupulated_data = data
+
+    return manupulated_data
+
+def getData(data):
+    print (data)
+
+    return model(data)
